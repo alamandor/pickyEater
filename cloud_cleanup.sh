a44b837cc1aabc5123c59779542a086550059ba4
alec <aag3@pdx.edu>
2021-05-28 05:01:02 +0000

    edited cleanup scripts to skip user interaction for faster teardown

--- cloud_cleanup.sh
+++ cloud_cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcloud run services delete picky-eater
-gcloud container images delete gcr.io/${GOOGLE_CLOUD_PROJECT}/picky-eater
+gcloud run services delete picky-eater --platform="managed" --region="us-west1" -q
+gcloud container images delete gcr.io/${GOOGLE_CLOUD_PROJECT}/picky-eater --force-delete-tags -q
