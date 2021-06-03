07309fc090b33807f2771a3a16907b435e5b1f71
alec <aag3@pdx.edu>
2021-06-03 23:34:38 +0000

    Created wrapper class for googleNLP API. Made function names clearer on what they accomplish and hide lower level functions away from caller. related to #2

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from yelpapi import YelpAPI
 from google.cloud import language
 import time
 
+
 class api:
     """Abstract base class that gets used by yelp and google apis"""
 
@@ -21,6 +22,7 @@ class Yelp(api):
 
     def getBusinesses(self):
         return self.businesses_search_results
+
     def getBusinessIds(self):
         return self.business_ids
 
@@ -43,7 +45,7 @@ class Yelp(api):
             self.business_ids.append(b_ids['id'])
 
     def getReviews(self, location, searchTerm):
-        self.searchBusinesses(location,searchTerm)
+        self.searchBusinesses(location, searchTerm)
         # Loop through businesses found in search query
         for b_ids in self.business_ids:
             # Ran into problems exceeding rate limit
@@ -58,3 +60,61 @@ class Yelp(api):
                         b['reviews'].append(text['text'])
 
         return self.getBusinesses()
+
+
+class GoogleNLP:
+    def __init__(self):
+        ## List to store resturants with sentiments
+        self.resturants_with_sentiments = []
+
+    def sentiment_analysis(self,businesses,analysis_term):
+        for business in businesses:
+            b_reviews = business['reviews']
+
+            client = language.LanguageServiceClient()
+            document = language.Document(
+                content=str(b_reviews),
+                type_=language.Document.Type.PLAIN_TEXT,
+            )
+            response = client.analyze_entity_sentiment(
+                document=document,
+                encoding_type='UTF32',
+            )
+            highest_sentiment = self.process_sentiments_with_resturants(
+                response, business['id'], business['name'], analysis_term)
+            sentiment_with_resturant = (
+                business['name'], business['lat'], business['long'], highest_sentiment)
+            # print(sentiment_with_resturant)
+            self.resturants_with_sentiments.append(sentiment_with_resturant)
+
+    def process_sentiments_with_resturants(self, sentiment_response, resturant_id, resturant_name, analysis_term):
+        """
+        Goes through the entities and sentiments from google NLP and combines the scores and the maginitudes of 
+        entities that mention the user-provided food, and retrieves the entity with the highest score.
+        """
+        highest = 0.0
+        for entity in sentiment_response.entities:
+            # Get the aggregate sentiment expressed for this entity in the provided document.
+            sentiment = entity.sentiment
+            magnitude = sentiment.magnitude
+            score = sentiment.score
+            combined_score = score*magnitude
+            if combined_score > highest and entity.name == analysis_term:
+                highest = combined_score
+                # print(u"resturant name: {}".format(resturant_name))
+                # print(u"resturant ID: {}".format(resturant_id))
+                # print(u"Representative name for the entity: {}".format(entity.name))
+                # print(u"Entity sentiment score: {}".format(sentiment.score))
+                # print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
+        
+        return highest
+    
+    def sort_tuples(self, sentiment_list, n):
+        """
+        Gets the top n resturants based on their NLP sentiment score, in DESC order.
+        """
+        sentiment_list.sort(key = lambda x: x[3] ,reverse=True)
+        return sentiment_list[:n]
+    
+    def get_top_sentiments(self, n):
+        return self.sort_tuples(self.resturants_with_sentiments,n)
--- review.py
+++ review.py
@@ -1,7 +1,7 @@
 from flask import redirect, request, url_for, render_template
 from flask.views import MethodView
 from google.cloud import language
-from api import Yelp
+from api import Yelp, GoogleNLP
 import time
 import sys
 import os
@@ -42,78 +42,33 @@ class Review(MethodView):
             yelp = Yelp(YELP_API_KEY)
             review_business_list = yelp.getReviews(u_location, food)                
 
+            googleNLP = GoogleNLP()
+            googleNLP.sentiment_analysis(review_business_list,food)
 
-            ## List to store resturants with sentiments
-            sentiment_tuple_list = []
-
-            ## Google natural language sentiment anaylsis
-            for business in review_business_list:
-                b_reviews = business['reviews']
-
-                client = language.LanguageServiceClient()    
-                document = language.Document(
-                    content=str(b_reviews),
-                    type_=language.Document.Type.PLAIN_TEXT,
-                )
-                response = client.analyze_entity_sentiment(
-                    document=document,
-                    encoding_type='UTF32',
-                )
-                highest_sentiment = self.process_sentiments_with_resturants(response, business['id'], business['name'], food)
-                sentiment_with_resturant = (business['name'], business['lat'], business['long'], highest_sentiment)
-                # print(sentiment_with_resturant)
-                sentiment_tuple_list.append(sentiment_with_resturant)
-            ## End of natural language API use
-
-            # Sort sentiments with resturant info in DESC order
-            top = self.sort_tuples(sentiment_tuple_list,3)
-            print("MADE THROUGH API CALLS")
-            print(top)
+            top_businesses = googleNLP.get_top_sentiments(3)
 
             # Storing new search in database
-            model.insert(u_location, food, top[0][0],top[0][1],top[0][2],top[0][3])
-            model.insert(u_location, food, top[1][0],top[1][1],top[1][2],top[1][3])
-            model.insert(u_location, food, top[2][0],top[2][1],top[2][2],top[2][3])
+            model.insert(u_location, food, top_businesses[0][0],top_businesses[0][1],top_businesses[0][2],top_businesses[0][3])
+            model.insert(u_location, food, top_businesses[1][0],top_businesses[1][1],top_businesses[1][2],top_businesses[1][3])
+            model.insert(u_location, food, top_businesses[2][0],top_businesses[2][1],top_businesses[2][2],top_businesses[2][3])
         
         # Or else we retrive from the cache in Datastore
         else:
-            top = []
+            top_businesses = []
             print("else statement")
             for item in table_results:
-                top.append([item['name'],item['lat'],item['lon'],item['sent'],])
+                top_businesses.append([item['name'],item['lat'],item['lon'],item['sent'],])
             
             print("RETRIEVED FROM CACHE")
-            top = self.sort_tuples(top, 3)
-            print(top)
+            top_businesses = self.sort_tuples(top_businesses, 3)
+            print(top_businesses)
 
         ## Send data to review.html and render it
-        return render_template('review.html', result=result,top=top, map=map, key=MAP_KEY)
-    
-    def process_sentiments_with_resturants(self, sentiment_response, resturant_id, resturant_name, food):
-        """
-        Goes through the entities and sentiments from google NLP and combines the scores and the maginitudes of 
-        entities that mention the user-provided food, and retrieves the entity with the highest score.
-        """
-        highest = 0.0
-        for entity in sentiment_response.entities:
-            # Get the aggregate sentiment expressed for this entity in the provided document.
-            sentiment = entity.sentiment
-            magnitude = sentiment.magnitude
-            score = sentiment.score
-            combined_score = score*magnitude
-            if combined_score > highest and entity.name == food:
-                highest = combined_score
-                # print(u"resturant name: {}".format(resturant_name))
-                # print(u"resturant ID: {}".format(resturant_id))
-                # print(u"Representative name for the entity: {}".format(entity.name))
-                # print(u"Entity sentiment score: {}".format(sentiment.score))
-                # print(u"Entity sentiment magnitude: {}".format(sentiment.magnitude))
-        
-        return highest
+        return render_template('review.html', result=result,top=top_businesses, map=map, key=MAP_KEY)
     
     def sort_tuples(self, sentiment_list, n):
         """
-        Gets the top n resturants based on their NLP sentiment score, in DESC order.
+        Gets the top_businesses n resturants based on their NLP sentiment score, in DESC order.
         """
         sentiment_list.sort(key = lambda x: x[3] ,reverse=True)
         return sentiment_list[:n]
